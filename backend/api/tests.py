e79ab9e5ff13207b2f59774bb5c86cf500bb72d6
klariss94 <klariss94@gmail.com>
2026-09-03 18:48:48 +0300

    Add tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,5 +1,6 @@
 # backend/api/tests.py
 from http import HTTPStatus
+
 from api import models
 from django.test import Client, TestCase
 
@@ -8,12 +9,10 @@ class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
 
-
     def test_list_exists(self):
         """Проверка доступности списка задач."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
 
     def test_task_creation(self):
         """Проверка создания задачи."""
